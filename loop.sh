9636b8635e3ca0532ea7006c3eab76cc15b7fe1d
madfibrous <jaskrol@gmail.com>
2022-05-22 10:51:12 -0700

    Changed loop params and NMB for first

--- loop.sh
+++ loop.sh
@@ -1,7 +1,7 @@
 #!/bin/csh
 foreach l(32 64 128 256)
 do
-    foreach g(1 2 4 8 16) 
+    foreach g(1 4 16 64 256 1024 4096 8192) 
     do
     /usr/local/apps/cuda/cuda-11.1/bin/nvcc -DLOCAL_SIZE=%l -DNUMB=%g -o arrayMul arrayMul.cu
     ./arrayMul
