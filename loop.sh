6e211fc6a51dc2fd2ba5f0af8a42d91e9d4c2b1b
madfibrous <jaskrol@gmail.com>
2022-05-22 11:22:26 -0700

    Loop sh in progress

--- loop.sh
+++ loop.sh
@@ -1,7 +1,7 @@
 #!/bin/csh
 
 foreach t (32 64 128 256)
-    foreach g (1 4 16 64 256 1024 4096 8192)
+    foreach g (1 4 16 64 256)
         g++ -o first first.cpp /usr/local/apps/cuda/10.1/lib64/libOpenCL.so.1.1 -lm -fopenmp -DLOCAL_SIZE=$t -DNMB=$g
         ./first
 end
