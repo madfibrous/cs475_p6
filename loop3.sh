1563ca027797d89e4ec2cb65ceec5492afb158ec
madfibrous <jaskrol@gmail.com>
2022-05-24 16:33:39 -0700

    Fixed addMulReduce output

--- loop3.sh
+++ loop3.sh
@@ -2,7 +2,7 @@
 
 for t in 32 64 256
 do
-    for g in 1 16 64 256 2048 8192
+    for g in 32 256 2048 8192 16384 32768 65536 131072 262144
     do
         g++ -o second second.cpp /usr/local/apps/cuda/10.1/lib64/libOpenCL.so.1.1 -lm -w -fopenmp -DLOCAL_SIZE=$t -DNMB=$g
         ./second
